5f0337902b38b1cea7c005317c6a910b6f561fb5
@DCaff14 <dmccaffrey@ithaca.edu>
2019-03-20 01:17:06 -0400

    added transcript entry tests

---
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/Transcript.java
@@ -0,0 +1,17 @@
+package ithacacollege.comp345.group4.classPlanner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Transcript {
+    List<TranscriptEntry> courseList;
+
+    public Transcript() {
+        courseList = new ArrayList<>();
+    }
+
+    public Transcript(String filename) {
+
+    }
+
+}
