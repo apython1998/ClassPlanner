e2a4090aeb0415a842b1fb798395f7f88ced4401
@DCaff14 <dmccaffrey@ithaca.edu>
2019-03-20 01:25:32 -0400

    added transcript entry tests

--- src/test/java/ithacacollege/comp345/group4/classPlanner/TranscriptTest.java
+++ src/test/java/ithacacollege/comp345/group4/classPlanner/TranscriptTest.java
@@ -1,4 +1,14 @@
 package ithacacollege.comp345.group4.classPlanner;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TranscriptTest {
+
+    @Test
+    public void constructorTest() {
+        Transcript myTranscript = new Transcript();
+        assertNotNull(myTranscript);
+        myTranscript = new Transcript("");
+    }
 }
