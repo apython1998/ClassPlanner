5fe4d35a4dc100b975bfe32fb7454c6fd2dff6a9
dshane <dylanshanee@gmail.com>
2019-03-20 09:52:55 -0400

    merged addCourses to master

---
+++ src/test/java/ithacacollege/comp345/group4/classPlanner/StudentTest.java
@@ -0,0 +1,69 @@
+package ithacacollege.comp345.group4.classPlanner;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentTest {
+
+    @Test
+    void addPastCoursesTest(){
+        Student student = new Student(12345678, null, null);
+        List<Course> courseList = new ArrayList<>();
+        Course course1 = new Course("test1", 12345, 3.0, "TEST123", "FA17", null);
+        Course course2 = new Course("test2", 13465, 3.0, "TEST321", "FA17", null);
+        Course course3 = new Course("test1", 12345, 3.0, "TEST123", "FA17", null);
+        courseList.add(course1);
+        courseList.add(course2);
+        courseList.add(course3);
+
+        student.addCoursesTaken(courseList);
+
+        List<Course> emptyList = new ArrayList<>();
+
+        assertEquals(courseList, student.getCoursesTaken());
+        assertNotEquals(emptyList, student.getCoursesTaken());
+    }
+
+    @Test
+    void addCurrentCoursesTest(){
+        Student student = new Student(12345678, null, null);
+        List<Course> courseList = new ArrayList<>();
+        Course course1 = new Course("test1", 12345, 3.0, "TEST123", "FA17", null);
+        Course course2 = new Course("test2", 13465, 3.0, "TEST321", "FA17", null);
+        Course course3 = new Course("test1", 12345, 3.0, "TEST123", "FA17", null);
+        courseList.add(course1);
+        courseList.add(course2);
+        courseList.add(course3);
+
+        student.addCurrentCourses(courseList);
+
+        List<Course> emptyList = new ArrayList<>();
+
+        assertEquals(courseList, student.getCurrentCourses());
+        assertNotEquals(emptyList, student.getCurrentCourses());
+    }
+
+    @Test
+    void addPlannedCoursesTest(){
+        Student student = new Student(12345678, null, null);
+        List<Course> courseList = new ArrayList<>();
+        Course course1 = new Course("test1", 12345, 3.0, "TEST123", "FA17", null);
+        Course course2 = new Course("test2", 13465, 3.0, "TEST321", "FA17", null);
+        Course course3 = new Course("test1", 12345, 3.0, "TEST123", "FA17", null);
+        courseList.add(course1);
+        courseList.add(course2);
+        courseList.add(course3);
+
+        student.addCoursesPlanned(courseList);
+
+        List<Course> emptyList = new ArrayList<>();
+
+        assertEquals(courseList, student.getCoursesPlanned());
+        assertNotEquals(emptyList, student.getCoursesPlanned());
+    }
+
+}
