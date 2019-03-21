da97ba0623f9823adba8b90c5f298d4b3803e333
josephcleveland <jcleveland@ithaca.edu>
2019-03-21 11:25:06 -0400

    Completed uploadMajorReqs, is passing test

--- src/main/java/ithacacollege/comp345/group4/classPlanner/model/Directory.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/model/Directory.java
@@ -2,9 +2,14 @@ package ithacacollege.comp345.group4.classPlanner.model;
 
 import ithacacollege.comp345.group4.classPlanner.InvalidArgumentException;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Directory {
 
@@ -13,6 +18,7 @@ public class Directory {
 
     public Directory() {
         this.students = new HashMap<>();
+        this.majorDirectory = new ArrayList<>();
     }
 
     public Directory(Map<String, User> users) {
@@ -38,7 +44,40 @@ public class Directory {
     }
 
     public void uploadMajor(String file){
+        JSONParser parser = new JSONParser();
+        Major newMajor = new Major();
+        try {
+            JSONObject majorObject = (JSONObject)parser.parse(new FileReader(file));
 
+            String title = (String) majorObject.get("title");
+            newMajor.title = title;
+
+            JSONArray courses = (JSONArray) majorObject.get("courses");
+            Iterator<String> courseitr = courses.iterator();
+            while (courseitr.hasNext()) {
+                String courseTitle = courseitr.next();
+                Course newCourse = new Course();
+                newCourse.setCourseDiscAndNum(courseTitle);
+                newMajor.addCourse(newCourse);
+            }
+
+            JSONArray grouping = (JSONArray) majorObject.get("grouping");
+            Iterator<JSONArray> groupingItr = grouping.iterator();
+            while(groupingItr.hasNext()){
+                JSONArray chooseOne = (JSONArray) groupingItr.next();
+                Iterator<String> chooseCourseItr = chooseOne.iterator();
+                List<Course> chooseCourseList = new ArrayList<>();
+                while(chooseCourseItr.hasNext()) {
+                    Course c = new Course();
+                    c.setCourseDiscAndNum(chooseCourseItr.next());
+                    chooseCourseList.add(c);
+                }
+                newMajor.addChooseOne(chooseCourseList);
+            }
+        }
+        catch(IOException e){e.printStackTrace();}
+        catch(ParseException e){e.printStackTrace();}
+        majorDirectory.add(newMajor);
     }
 
     /**************************** GETTERS AND SETTERS     ****************************/
