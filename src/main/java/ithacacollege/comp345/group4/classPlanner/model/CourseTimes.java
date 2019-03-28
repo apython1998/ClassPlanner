f498c388ff811b5eb0eb4235a47b11ede774526a
@DCaff14 <dmccaffrey@ithaca.edu>
2019-03-28 10:40:03 -0400

    creating course times object, adding functionality to Course

--- src/main/java/ithacacollege/comp345/group4/classPlanner/model/CourseTimes.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/model/CourseTimes.java
@@ -1,4 +1,47 @@
 package ithacacollege.comp345.group4.classPlanner.model;
 
-public class CourseTime {
+import java.util.List;
+
+public class CourseTimes {
+    List<Character> days;
+    List<String> times;
+
+    public CourseTimes(List<Character> day, List<String> time) {
+        this.days = day;
+        this.times = time;
+    }
+
+    public void addDay(char dayIn) {
+        days.add(dayIn);
+    }
+
+    public void addTime(String timeIn) {
+        times.add(timeIn);
+    }
+
+    public void delDay(char dayIn) {
+        days.remove(dayIn);
+    }
+
+    public void delTime(String timeIn) {
+        times.remove(timeIn);
+    }
+
+    public void setDay(List<Character> day) {
+        this.days = day;
+    }
+
+    public void setTime(List<String> time) {
+        this.times = time;
+    }
+
+    public List<Character> getDays() {
+        return days;
+    }
+
+    public List<String> getTimes() {
+        return times;
+    }
+
+
 }
--- src/main/java/ithacacollege/comp345/group4/classPlanner/model/Section.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/model/Section.java
@@ -1,4 +1,46 @@
 package ithacacollege.comp345.group4.classPlanner.model;
 
-public class Section {
+import java.util.List;
+
+public class Section extends Course {
+    private int num;
+    private String year;
+    private CourseTimes times;
+
+
+    Section() {
+        super();
+        num = 0;
+        times = null;
+        year = "";
+    }
+
+    Section(String name, int crn, double credits, String courseDiscAndNum, String semester, List<Course> preReqs, int num, CourseTimes times, String year) {
+        super(name, crn, credits, courseDiscAndNum, semester, preReqs);
+        this.num = num;
+        this.times = times;
+        this.year = year;
+    }
+
+    public void addCourseTimes(String courseTimes) {
+        String[] subStringList = courseTimes.split(" ");
+        String dayString = subStringList[0];
+        for (int i = 0; i < dayString.length(); i++) {
+            times.addDay(dayString.charAt(0));
+        }
+        for (int i = 1; i < subStringList.length; i++) {
+            times.addTime(subStringList[i]);
+        }
+    }
+
+    public void delCourseTimes(String courseTimes) {
+        String[] subStringList = courseTimes.split(",");
+        String dayString = subStringList[0];
+        for (int i = 0; i < dayString.length(); i++) {
+            times.delDay(dayString.charAt(0));
+        }
+        for (int i = 1; i < subStringList.length; i++) {
+            times.delTime(subStringList[i]);
+        }
+    }
 }
