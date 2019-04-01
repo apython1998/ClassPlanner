ac551743f677e026e41ae8c368359206ce56a171
@DCaff14 <dmccaffrey@ithaca.edu>
2019-03-31 22:09:37 -0400

    set up basic functionality for course times

--- src/main/java/ithacacollege/comp345/group4/classPlanner/model/CourseTimes.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/model/CourseTimes.java
@@ -1,46 +1,96 @@
 package ithacacollege.comp345.group4.classPlanner.model;
 
+import ithacacollege.comp345.group4.classPlanner.InvalidArgumentException;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class CourseTimes {
-    List<Character> days;
-    List<String> times;
 
-    public CourseTimes(List<Character> day, List<String> time) {
-        this.days = day;
-        this.times = time;
+public class CourseTimes {
+    private static final String validDays = "MTWRF";
+    private List<Character> days;
+    private String time;
+
+    public CourseTimes() {
+        days = new ArrayList<>();
+        time = "";
+    }
+
+    public CourseTimes(String days, String time) {
+        this.days = new ArrayList<>();
+        for (int i = 0; i < days.length(); i++) {
+            char currDay = days.charAt(i);
+            addDay(currDay);
+        }
+        if (isTimeValid(time)) {
+            this.time = time;
+        } else {
+            throw new InvalidArgumentException("Time is invalid");
+        }
     }
 
     public void addDay(char dayIn) {
-        days.add(dayIn);
+        if (isDayValid(dayIn)) {
+            if (!this.days.contains(dayIn)) {
+                this.days.add(dayIn);
+            }
+        } else {
+            throw new InvalidArgumentException("Days must be M, T, W, R, or F");
+        }
     }
 
-    public void addTime(String timeIn) {
-        times.add(timeIn);
+    public void removeDay(char dayIn) {
+        if (isDayValid(dayIn)) {
+            if (this.days.contains(dayIn)) {
+                this.days.remove(dayIn);
+            }
+        } else {
+            throw new InvalidArgumentException("Days must be M, T, W, R, or F");
+        }
     }
 
-    public void delDay(char dayIn) {
-        days.remove(dayIn);
-    }
-
-    public void delTime(String timeIn) {
-        times.remove(timeIn);
-    }
-
-    public void setDay(List<Character> day) {
-        this.days = day;
-    }
-
-    public void setTime(List<String> time) {
-        this.times = time;
+    public void setTime(String timeIn) {
+        time = timeIn;
     }
 
     public List<Character> getDays() {
         return days;
     }
 
-    public List<String> getTimes() {
-        return times;
+    public char getDay(int index) {
+        return days.get(index);
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public static boolean isDayValid(char currDay) {
+        return validDays.contains(Character.toString(currDay));
+    }
+
+    public static boolean isTimeValid(String time) {
+        if (time.length() > 5 || time.length() < 4 || time.indexOf(':') == -1) {
+            return false;
+        } else {
+            String[] subStringList = time.split(":");
+            if (subStringList.length > 2) {
+                return false;
+            }
+            int hours = Integer.parseInt(subStringList[0]);
+            int minutes = Integer.parseInt(subStringList[1]);
+            return !(hours < 0 || hours > 23 || minutes < 0 || minutes > 59);
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < days.size(); i++) {
+            sb.append(days.get(i));
+        }
+        sb.append(" " + time);
+        return sb.toString();
     }
 
 
--- src/main/java/ithacacollege/comp345/group4/classPlanner/model/Section.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/model/Section.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Section extends Course {
     private int num;
     private String year;
-    private CourseTimes times;
+    private List<CourseTimes> times;
 
 
     Section() {
@@ -18,29 +18,27 @@ public class Section extends Course {
     Section(String name, int crn, double credits, String courseDiscAndNum, String semester, List<Course> preReqs, int num, CourseTimes times, String year) {
         super(name, crn, credits, courseDiscAndNum, semester, preReqs);
         this.num = num;
-        this.times = times;
+//        this.times = times;
         this.year = year;
     }
 
     public void addCourseTimes(String courseTimes) {
+        CourseTimes newTime = new CourseTimes();
         String[] subStringList = courseTimes.split(" ");
         String dayString = subStringList[0];
+        String timeString = subStringList[1];
         for (int i = 0; i < dayString.length(); i++) {
-            times.addDay(dayString.charAt(0));
+            newTime.addDay(dayString.charAt(i));
         }
-        for (int i = 1; i < subStringList.length; i++) {
-            times.addTime(subStringList[i]);
-        }
+        newTime.setTime(timeString);
+        times.add(newTime);
     }
 
     public void delCourseTimes(String courseTimes) {
-        String[] subStringList = courseTimes.split(",");
-        String dayString = subStringList[0];
-        for (int i = 0; i < dayString.length(); i++) {
-            times.delDay(dayString.charAt(0));
-        }
-        for (int i = 1; i < subStringList.length; i++) {
-            times.delTime(subStringList[i]);
-        }
+//        String[] subStringList = courseTimes.split(",");
+//        String dayString = subStringList[0];
+//        for (int i = 0; i < dayString.length(); i++) {
+////            times.delDay(dayString.charAt(0));
+//        }
     }
 }
