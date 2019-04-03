d750eec82ec87edb22006eb6dfe265876f50550e
josephcleveland <jcleveland@ithaca.edu>
2019-04-02 21:47:20 -0400

    Added enum for Semester Started skeleton for Plan and Semester plan

--- src/main/java/ithacacollege/comp345/group4/classPlanner/model/Plan.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/model/Plan.java
@@ -1,4 +1,30 @@
 package ithacacollege.comp345.group4.classPlanner.model;
 
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plan {
+
+    private List<SemesterPlan> semesterPlans;
+
+    public Plan(int numSemesters, float creditsPerSemester, Semester startSemester, Year startYear){
+        semesterPlans = new ArrayList<>();
+        generatePlan(numSemesters, creditsPerSemester, startSemester, startYear);
+    }
+
+    /**
+     * Fills out a sequence of semester plans.
+     * This plan will be in accordance with the degree requirements and will not violate prerequisite rules.
+     * Semesters alternate between Spring and Fall, and year increases every new spring semester.
+     *
+     * @param numSemesters Number of semester plans to create
+     * @param creditsPerSemester Number of credits per semester
+     * @param startSemester First semester to generate
+     * @param startYear First year to generate
+     */
+    private void generatePlan(int numSemesters, float creditsPerSemester, Semester startSemester, Year startYear){
+
+    }
+
 }
--- src/main/java/ithacacollege/comp345/group4/classPlanner/model/SemesterPlan.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/model/SemesterPlan.java
@@ -1,4 +1,12 @@
 package ithacacollege.comp345.group4.classPlanner.model;
 
+import java.time.Year;
+
 public class SemesterPlan {
+    Semester semester;
+    Year year;
+
+    public SemesterPlan() {
+
+    }
 }
