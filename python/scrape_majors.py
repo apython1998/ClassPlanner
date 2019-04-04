2f2bea9b608624d7e870e1ed247af50bad31a649
apython1998 <alex.python1998@gmail.com>
2019-04-04 13:27:40 -0400

    Scraping majors in progress, generating json with major title and type

---
+++ python/scrape_majors.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+import time
+import json  # Use is json.dumps(object)
+
+
+def scrape_major(url):                                              # Scrape Data for a Single Major
+    major = dict()
+    major_title = ''                                                # Title for Major
+    major_type = ''                                                 # Type of Major
+    major_required_courses = []                                     # Required Classes
+    major_required_choose_ones = []                                 # Required ChooseOnes
+    if 'sports-media' in url:                                       # The only major that doesn't list its type in URL
+        major_title = 'Sports Media'
+        major_type = 'BS'
+    else:                                                           # Loop works for all other links
+        major_url_title_type_section = url.split('/')[-2]           # Extract major title and type from url
+        if major_url_title_type_section[0] is '-':                  # Fixes error where url starts with -
+            major_url_title_type_section = major_url_title_type_section[1:]
+        major_title_type_split = major_url_title_type_section.split('-')
+        if 'teaching' in major_title_type_split:                    # Loop for handling majors with a teaching option
+            major_type_temp = major_title_type_split[-3]
+            major_title_type_split.remove(major_type_temp)
+            major_title_type_split.append(major_type_temp)
+        major_type = major_title_type_split[-1].upper()             # Get & Capitalize the type of major
+        for i in range(len(major_title_type_split[0:-1])):
+            major_title_type_split[i] = major_title_type_split[i][0].upper() + major_title_type_split[i][1:]
+        major_title = " ".join(major_title_type_split[0:-1])        # Generate the title of the major
+    # browser = webdriver.Chrome()
+    # browser.get(url)
+    # content_div = browser.find_element_by_id('content')
+    # browser.close()
+    major['title'] = major_title.strip()                            # Populate the Dictionary
+    major['type'] = major_type.strip()                              # ...
+    major['courses'] = major_required_courses                       # ...
+    major['grouping'] = major_required_choose_ones                  # ...
+    return major                                                    # Return the major
+
+
+def scrape_major_urls(url):  # Scrape URLs for every Major
+    major_urls = []
+    browser = webdriver.Chrome()
+    browser.get(url)
+    pass  # Do shit
+    content_div = browser.find_element_by_id('textcontainer')
+    major_links = content_div.find_elements_by_tag_name('a')
+    for major_link in major_links:
+        href = major_link.get_attribute('href')
+        if href is not None:
+            major_urls.append(href)
+    browser.close()
+    return major_urls
+
+
+def main():
+    major_urls = scrape_major_urls('https://catalog.ithaca.edu/undergrad/programsaz/undergraduate-degree/')
+    majors = []
+    for major_url in major_urls:
+        majors.append(scrape_major(major_url))  # Add a dictionary representing major requirements
+        # time.sleep(1)                           # Keep IP from getting blacklisted
+    with open('../src/main/resources/majorCatalog.json', 'w') as outfile:
+        json.dump(majors, outfile)
+
+
+main()
