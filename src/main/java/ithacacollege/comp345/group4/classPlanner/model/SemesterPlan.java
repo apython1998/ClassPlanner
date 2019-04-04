780b613dd091ce385682ccaa075a009b1aa83b79
josephcleveland <jcleveland@ithaca.edu>
2019-04-03 22:24:39 -0400

    Continued work on tests

--- src/main/java/ithacacollege/comp345/group4/classPlanner/model/SemesterPlan.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/model/SemesterPlan.java
@@ -1,19 +1,19 @@
 package ithacacollege.comp345.group4.classPlanner.model;
 
+import java.sql.Time;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SemesterPlan {
-    Semester semester;
-    Year year;
+    private Semester semester;
+    private int year;
     private List<Course> courses;
 
-    public SemesterPlan(Semester semester, Year year, List<SemesterPlan> previousSemesters, Major major, Transcript transcript) {
+    public SemesterPlan(Semester semester, int year, List<SemesterPlan> previousSemesters, Major major, Transcript transcript) {
         courses = new ArrayList<>();
         this.semester = semester;
         this.year = year;
-        generateSemesterPlan(previousSemesters, major, transcript);
     }
 
     /**
@@ -23,15 +23,29 @@ public class SemesterPlan {
      * @param major Major to check degree requirements
      * @param transcript Transcript, also to check for preReqs
      */
-    private void generateSemesterPlan(List<SemesterPlan> previousSemesters, Major major, Transcript transcript){
+    public void generateSemesterPlan(List<SemesterPlan> previousSemesters, Major major, Transcript transcript){
         //TODO
     }
 
-    private Course findCourseInCatalog(){
+    /**
+     * Finds from the catalog a course not yet present in the schedule, previous schedules, or transcript
+     * And which has all preReqs
+     * @param previousSemesters List of all previous plans
+     * @param major The major, for degree requirments
+     * @param transcript The student's transcript
+     * @return Course to be added to plan
+     */
+    public Course findNewCourseInCatalog(List<SemesterPlan> previousSemesters, Major major, Transcript transcript){
         return null;//TODO
     }
 
-    private boolean hasAllPreReqs(List<SemesterPlan> previousSemesters, Transcript transcript){
+    /**
+     * Determines if student has all preReqs for given course
+     * @param previousSemesters To check if preReqs may be present in previous plans
+     * @param transcript To check if preReqs may be present in transcript
+     * @return True if all preReqs are found
+     */
+    public boolean hasAllPreReqs(Course course, List<SemesterPlan> previousSemesters, Transcript transcript){
         return false;//TODO
     }
 
--- src/test/java/ithacacollege/comp345/group4/classPlanner/model/PlanTest.java
+++ src/test/java/ithacacollege/comp345/group4/classPlanner/model/PlanTest.java
@@ -2,6 +2,12 @@ package ithacacollege.comp345.group4.classPlanner.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+
 public class PlanTest {
 
     @Test
@@ -10,7 +16,30 @@ public class PlanTest {
     }
 
     @Test
-    public void semesterPlanConstructorTest(){
+    public void generateSemesterPlanTest(){
+
+    }
+
+    @Test
+    public void findNewCourseInCatalogTest(){
+
+    }
+
+    @Test
+    public void hasAllPreReqsTest(){
+        Transcript exTranscript = new Transcript("src/test/resources/exTranscript.json");
+        List<SemesterPlan> planList = new ArrayList<>();
+        SemesterPlan exPrevSemPlan = null;
+        SemesterPlan exCurrSemPlan = null;
+        Course exCourse1 = new Course();
+        try {
+            exPrevSemPlan = JsonUtil.fromJsonFile("src/test/resources/exPrevSemPlan.json", SemesterPlan.class);
+            planList.add(exPrevSemPlan);
+            exCurrSemPlan = JsonUtil.fromJsonFile("src/test/resources/exCurrSemPlan.json", SemesterPlan.class);
+
+        }catch(IOException e){ e.printStackTrace();}
+
+        assertTrue(exCurrSemPlan.hasAllPreReqs(exCourse1, planList, exTranscript));
 
     }
 }
