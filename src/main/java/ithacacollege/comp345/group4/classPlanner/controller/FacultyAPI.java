1ec4551d6b2da62d3383682ccd760bb8549d03f3
apython1998 <alex.python1998@gmail.com>
2019-04-18 13:49:31 -0400

    Registration and login are handled by FacultyAPI now, tests passing

--- src/main/java/ithacacollege/comp345/group4/classPlanner/controller/FacultyAPI.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/controller/FacultyAPI.java
@@ -1,6 +1,7 @@
 package ithacacollege.comp345.group4.classPlanner.controller;
 
 import ithacacollege.comp345.group4.classPlanner.model.Directory;
+import ithacacollege.comp345.group4.classPlanner.model.Faculty;
 
 public class FacultyAPI {
 
@@ -12,4 +13,19 @@ public class FacultyAPI {
         this.directory = directory;
     }
 
+    /**
+     * Takes info from UI and creates a faculty account
+     * @param username
+     * @param password
+     * @return
+     */
+    public boolean register(String username, String password) { return directory.registerFaculty(username, password); }
+
+    /**
+     * Takes a login attempt and checks if user is authenticated
+     * @param username
+     * @param password
+     * @return Faculty object if login is successful, otherwise null
+     */
+    public Faculty login(String username, String password) { return directory.loginFaculty(username, password); }
 }
