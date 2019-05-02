944fab7b0af31dfaac0bcd857d5451397a2e0cae
apython1998 <alex.python1998@gmail.com>
2019-05-02 11:28:10 -0400

    Reset db again, tests passing

--- src/main/java/ithacacollege/comp345/group4/classPlanner/ClassPlannerMain.java
+++ src/main/java/ithacacollege/comp345/group4/classPlanner/ClassPlannerMain.java
@@ -22,54 +22,26 @@ public class ClassPlannerMain {
 //        for (Course course : allCourses) {
 //            courseCatalog.put(course.getCourseNum(), course);
 //        }
-////        Map<String, List<Section>> sectionCatalog = new HashMap<>();
-////        String courseTimes1 = "MWF 9:00-9:50,TR 8:00-9:15";
-////        String courseTimes2 = "MWF 10:00-10:50,TR 9:25-10:40";
-////        String courseTimes3 = "MWF 11:00-11:50,TR 10:50-12:05";
-////        String courseTimes4 = "MWF 12:00-12:50,TR 2:35-3:50";
-////        for (Course course : allCourses) {
-////            List<Section> thisSectionList = new ArrayList<>();
-////            courseCatalog.put(course.getCourseNum(), course);
-////            Section s1 = new Section(course, 1, "24601", "F2019", courseTimes1);
-////            Section s2 = new Section(course, 2, "24601", "F2019", courseTimes2);
-////            Section s3 = new Section(course, 3, "24601", "F2019", courseTimes3);
-////            Section s4 = new Section(course, 4, "24601", "F2019", courseTimes4);
-////
-////            thisSectionList.add(s1);
-////            thisSectionList.add(s2);
-////            thisSectionList.add(s3);
-////            thisSectionList.add(s4);
-////
-////            sectionCatalog.put(course.getCourseNum(), thisSectionList);
-////        }
-//        /**
-//         * Load the Sections from a JSON file using JSON Util
-//         */
-//        List<Section> allSections = JsonUtil.listFromJsonFile("src/main/resources/sectionsCatalog.json", Section.class);
 //        Map<String, List<Section>> sectionCatalog = new HashMap<>();
-//        List<Section> list1 = allSections.subList(0, 2);
-//        List<Section> list2 = new ArrayList<>();
-//        Course course1 = courseCatalog.get("COMP11500");
-//        for (Section s: list1) {
-//            list2.add(s.setCourse(course1));
-//        }
-//        sectionCatalog.put("COMP11500", list2);
+//        String courseTimes1 = "MWF 9:00-9:50,TR 8:00-9:15";
+//        String courseTimes2 = "MWF 10:00-10:50,TR 9:25-10:40";
+//        String courseTimes3 = "MWF 11:00-11:50,TR 10:50-12:05";
+//        String courseTimes4 = "MWF 12:00-12:50,TR 2:35-3:50";
+//        for (Course course : allCourses) {
+//            List<Section> thisSectionList = new ArrayList<>();
+//            courseCatalog.put(course.getCourseNum(), course);
+//            Section s1 = new Section(course, 1, "24601", "F2019", courseTimes1);
+//            Section s2 = new Section(course, 2, "24601", "F2019", courseTimes2);
+//            Section s3 = new Section(course, 3, "24601", "F2019", courseTimes3);
+//            Section s4 = new Section(course, 4, "24601", "F2019", courseTimes4);
 //
-//        list1 = allSections.subList(2, 4);
-//        List<Section> list3 = new ArrayList<>();
-//        Course course2 = courseCatalog.get("MATH14400");
-//        for (Section s: list1) {
-//            list3.add(s.setCourse(course2));
-//        }
-//        sectionCatalog.put("MATH14400", list3);
+//            thisSectionList.add(s1);
+//            thisSectionList.add(s2);
+//            thisSectionList.add(s3);
+//            thisSectionList.add(s4);
 //
-//        list1 = allSections.subList(4, 6);
-//        List<Section> list4 = new ArrayList<>();
-//        Course course3 = courseCatalog.get("MATH11100");
-//        for (Section s: list1) {
-//            list3.add(s.setCourse(course3));
+//            sectionCatalog.put(course.getCourseNum(), thisSectionList);
 //        }
-//        sectionCatalog.put("MATH11100", list4);
 //
 //
 //        /**
